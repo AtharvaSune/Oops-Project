a21a94b7a76d96ec7fdc8236e826427163e308fe
Ameya <salankaras1234@gmail.com>
2018-11-19 13:42:55 +0530

    Search page variables renamed

--- src/oops/Oops.java
+++ src/oops/Oops.java
@@ -20,7 +20,7 @@ import Login.LoginPage;
     public static void main(String[] args) {
         // TODO code application logic here
         new LoginPage();
-        //        try {
+//      try {
 //            Class.forName("com.mysql.jdbc.Driver");
 //            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Hoteldb", "root", "");
 //            Statement stmt = con.createStatement();
